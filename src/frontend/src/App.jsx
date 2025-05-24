d8074e3c9c9a0c75667ca6e761154a17a2167d3e
newcoder789 <aryandixit984@gmail.com>
2025-05-25 00:01:39 +0530

    frontend part fixed

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -6,24 +6,29 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from "react";
 
+
+import { IdentityKitProvider, ConnectWallet } from "@nfid/identitykit/react";
+import "@nfid/identitykit/react/styles.css";
+
 export default function App() {
   useEffect(() => {
     AOS.init({
       duration: 1500,
       once: true,
-    })
-  })
+    });
+  }, []);
+  
   return (
-    <main>
-      {/*Gradient image*/}
-      <img className="absolute top-0 right-0 opacity-60 -z-10  " src="/images/gradient.png" alt="Gradient-img" />
-      {/*Blur effect */}
-      <div className="absolute top-[30%] right-[5%] w-[30rem] h-[30rem] rounded-full bg-[#e99b63] opacity-40 blur-[200px] -z-10"></div>
-      <div className="absolute top-[35%] right-[10%] w-[15rem] h-[15rem] rounded-full bg-[#f7ab6c] opacity-60 blur-[100px] -z-10"></div>
+      <main>
+        {/*Gradient image*/}
+        <img className="absolute top-0 right-0 opacity-60 -z-10  " src="/images/gradient.png" alt="Gradient-img" />
+        {/*Blur effect */}
+        <div className="absolute top-[30%] right-[5%] w-[30rem] h-[30rem] rounded-full bg-[#e99b63] opacity-40 blur-[200px] -z-10"></div>
+        <div className="absolute top-[35%] right-[10%] w-[15rem] h-[15rem] rounded-full bg-[#f7ab6c] opacity-60 blur-[100px] -z-10"></div>
 
 
-      <Header />
-      <Hero />
-    </main>
+        <Header />
+        <Hero />
+      </main>
   )
 }
