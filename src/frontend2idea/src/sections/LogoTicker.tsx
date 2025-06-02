2af493e6c0d1f0fc143e6aea68ecb3d4accdfeb3
Ayush4404 <143592428+Ayush4404@users.noreply.github.com>
2025-06-03 01:50:02 +0530

    Update LogoTicker.tsx

--- src/frontend2idea/src/sections/LogoTicker.tsx
+++ src/frontend2idea/src/sections/LogoTicker.tsx
@@ -5,16 +5,17 @@ import pulseLogo from '@/assets/logo-pulse.png';
 import apexLogo from '@/assets/logo-apex.png';
 import Image from 'next/image';
 export const LogoTicker = () => {
-  return<div>
+  return<div className='py-8 md:p-12 bg-white'>
    <div className='container'>
-    <div>
+    <div className='flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]'>
+    <div className='flex gap-14 flex-none'>
       <Image src={achmeLogo} alt='Acme Logo'className='logo-ticker-image'/>
       <Image src={quantumLogo} alt='Quantum Logo' className='logo-ticker-image'/>
       <Image src={celestialLogo} alt='Celestial Logo'className='logo-ticker-image'/>
       <Image src={pulseLogo} alt='pulse Logo'className='logo-ticker-image'/>
       <Image src={apexLogo} alt='Apex Logo'className='logo-ticker-image'/>
     </div>
-
+    </div>
     </div>
    </div> ;
 };
